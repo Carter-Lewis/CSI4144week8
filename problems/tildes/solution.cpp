631c672a09c1e75499d1137fce730262f628a2dd
Carter Lewis <carter_lewis1@baylor.edu>
2026-03-16 21:23:30 -0500

    Finished tildes

--- problems/tildes/solution.cpp
+++ problems/tildes/solution.cpp
@@ -5,11 +5,54 @@
 
 using namespace std;
 
+int parent[1000005];
+int sz[1000005];
+
+int find(int x) {
+  if(parent[x] != x) {
+    parent[x] = find(parent[x]);
+  }
+  return parent[x];
+}
+
+void unite(int a, int b) {
+  a = find(a);
+  b = find(b);
+
+  if(a != b) {
+    if(sz[a] < sz[b]) swap(a, b);
+    parent[b] = a;
+    sz[a] += sz[b];
+  }
+}
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+  int n, q;
+  cin >> n >> q;
 
-    // TODO: Implement solution
+  for(int i = 1; i < 1000005; i++) {
+    sz[i] = 1;
+    parent[i] = i;
+  }
 
-    return 0;
+  while (q-->0) {
+    char cmd;
+    cin >> cmd;
+    int a, b;
+
+    switch(cmd) {
+      case 't':
+        cin >> a >> b;
+        unite(a, b);
+        break;
+      case 's':
+        cin >> a;
+        cout << sz[find(a)] << endl;
+        break;
+      default:
+        cout << "something bad happened" << endl;
+        break;
+    }
+  }
+  return 0;
 }
